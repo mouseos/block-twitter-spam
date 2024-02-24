2ddf906fbbf4783e1727528f1d21272baab8ee93
Awayume <dev@awayume.jp>
2024-02-25 02:38:01 +0900

    曖昧なthisを置き換え

--- content.js
+++ content.js
@@ -2,7 +2,7 @@ const s1 = document.createElement('script');
 s1.src = chrome.runtime.getURL('./script/detect_lang.js');
 s1.addEventListener('load', function() {
   console.log('loaded');
-  this.remove();
+  s1.remove();
 });
 (document.head || document.documentElement).append(s1);
 
@@ -11,6 +11,6 @@ const s3 = document.createElement('script');
 s3.src = chrome.runtime.getURL('./script/script.js');
 s3.addEventListener('load', function() {
   console.log('loaded');
-  this.remove();
+  s3.remove();
 });
 (document.head || document.documentElement).append(s3);
